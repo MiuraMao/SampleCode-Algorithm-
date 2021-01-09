47674dedfa52cbe9ffa4ca02ad29923a83866ee9
MiuraMao <m_miura06@outlook.com>
2021-01-09 18:47:41 +0900

    Commit

--- List2.1.c
+++ List2.1.c
@@ -1,1 +1,22 @@
-//Commit
+#include <stdio.h>
+
+struct 
+{
+    int key;
+    int data;
+}table[100];
+
+int n;      /*tableに登録されているデータの個数*/
+int serch(int key)
+{
+    int i;
+    
+    i = 0;
+    while(i < n){
+        if (table[i].key == key)
+            return (table[i].data);     /*見つかった*/
+        i++;
+    }
+    return -1;                          /*見つからなかった*/
+}
+
