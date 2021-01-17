90b7ad42e82312fcc4121a9c3d1580eed2daac36
MiuraMao <m_miura06@outlook.com>
2021-01-17 20:21:31 +0900

    Ｃｏｍｍｉｔ

---
+++ List5.1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/*連結リストへの挿入*/
+struct CELL{
+    struct CELL *next;
+};
+
+int main(void){
+/*ポインタxで指されたセルの直後に新しいセルを挿入する*/
+struct CELL *p;
+struct CELL *x;
+
+if ((p = malloc(sizeof(struct CELL))) == NULL)
+    fatal_error("メモリがたりない");
+    //セルに値をセットする
+    p->next = x->next;
+    x->next = p;
+
+/*リストの先頭に新しいセルを挿入する*/
+    struct CELL *header, *p;
+    if ((p = malloc(sizeof(struct CELL))) == NULL)
+    //セルに値をセットする
+    p->next = header;
+    header = p;
+
+}
