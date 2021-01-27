941dae5af9282cbe982c51ad09de186a761271a3
MiuraMao <m_miura06@outlook.com>
2021-01-28 07:38:59 +0900

    Commit

---
+++ List9.2.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/*二分探索木に要素を挿入する関数insert*/
+
+typedef struct node {
+    KEY data;
+    struct node *left, *right;
+}NODE;
+//適当な型で宣言
+typedef int KEY;
+//グローバル変数rootを初期化
+NODE *root = NULL;
+
+NODE *insert(KEY key)
+{
+    NODE **p, *new;
+
+    p = &root;
+    while (*p !=NULL){
+        if (keyequal(key, (*p)->data))
+            return NULL;
+        else if (keylt(key, (*p)->data))
+            p = &(*p)->left;
+        else
+            p = &(*p)->right;
+    }
+
+    if ((new = malloc(sizeof(NODE))) == NULL)
+        error("out of memory!");
+    new->left = NULL;
+    new->right = NULL;
+    new->data = key;
+    *p = new;
+    return new;
+}
