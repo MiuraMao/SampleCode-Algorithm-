324770ade430dd55f155714d1b5234997cd4e6d5
MiuraMao <m_miura06@outlook.com>
2021-02-06 18:25:46 +0900

    Commit

---
+++ List16.1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/*探索木を利用した整列アルゴリズムのスケッチ*/
+//平衡木ではinsertとdelete_minをO(log n)で実行できる
+
+
+sort (int a[], int n)
+{
+    int i;
+
+    /*全ての要素を探索木に挿入する*/
+    for (i = 0; i < n; i++)
+        insert(a[i]);
+    
+    /*要素をキーが小さい順に取り出す*/
+    for (i = 0; i < n; i++)
+        a[i] = delete_min();
+ }
